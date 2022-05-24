85bb37ecb9a337a4d17cbeac1aca1c3640eb6db3
fernando.avanzo <fernando.avanzo@gmail.com>
2022-05-24 09:54:30 -0300

    Book-Clojure-For-The-Brave-And-True:ch06: name space code

--- src/book/chapter06/ch06_codes.clj
+++ src/book/chapter06/ch06_codes.clj
@@ -9,3 +9,19 @@
 
 ;that code works in any namespace
 (deref #'book.chapter06.ch06-codes/great-books)
+
+;namespace creation examples and test 'clojure.core/refer
+(in-ns 'cheese.taxonomy)
+
+(def cheddars ["mild" "medium" "strong" "sharp" "extra sharp"])
+(def bries ["Wisconsin" "Somerset" "Brie de Meaux" "Brie de Melun"])
+
+(in-ns 'cheese.analysis)
+
+(in-ns 'cheese.analysis)
+(clojure.core/refer-clojure)
+
+;; Notice the dash after "defn"
+(defn- private-function
+  "Just an example function that does nothing"
+  [])
